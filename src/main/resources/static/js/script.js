759760f14ae7ae51a81ea7a41c1fbec32289da35
Micheal Akintola <makintola@idealswift.com>
2025-01-27 08:44:03 +0100

    generated self-signed ssl certificate and configured application.properties to load on port 8443

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:3001/api/v1";
+const API_BASE_URL = "https://localhost:8443/api/v1";
 const BEARER_TOKEN = '';
 const FORMATTER = new Intl.NumberFormat('en-US', {
     style: 'currency',
